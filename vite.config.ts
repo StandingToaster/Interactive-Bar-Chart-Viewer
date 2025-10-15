6d10015e31f5bd6a1a83fe6d8235b6c72c064487
Harsimran Kalsi <hs2kalsi@uwaterloo.ca>
2025-10-15 19:57:18 -0400

    Fix SimpleKit requirements for Pages build

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   base: "/Interactive-Bar-Chart-Viewer/",
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/vite/client-[hash].js",
+      },
+    },
+  },
 });
